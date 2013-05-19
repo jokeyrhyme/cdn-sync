f34cda53a7dc39ac195f0871b752ad2d830abdf1
Ron Waldon <jokeyrhyme@gmail.com>
2013-05-19 14:05:33 +1000

    new Action prototype with initial tests

---
+++ test/action_test.js
@@ -0,0 +1,53 @@
+/*jslint es5:true, indent:2, maxlen:80, node:true*/
+/*global suite:true, test:true, suiteSetup:true, suiteTeardown:true, setup:true,
+ teardown:true*/ // Mocha
+'use strict';
+
+// Node.JS standard modules
+
+// 3rd-party modules
+
+var chai, assert, sinon;
+
+chai = require('chai');
+chai.use(require('sinon-chai'));
+assert = require('chai').assert;
+sinon = require('sinon');
+
+// custom modules
+
+// promise-bound anti-callbacks
+
+// this module
+
+suite('Action module', function () {
+
+  test('requires without issue', function () {
+    var Action = require('../lib/action');
+    assert.isFunction(Action, 'got Action constructor');
+  });
+
+});
+
+suite('Action constructor', function () {
+  var Action = require('../lib/action');
+
+  test('throws error for doUpload with no file', function () {
+    assert.throws(function () {
+      var action = new Action({ doUpload: true });
+    }, Error);
+  });
+
+  test('throws error for doHeaders with no file', function () {
+    assert.throws(function () {
+      var action = new Action({ doHeaders: true });
+    }, Error);
+  });
+
+  test('throws error for doDelete with no path', function () {
+    assert.throws(function () {
+      var action = new Action({ doDelete: true });
+    }, Error);
+  });
+
+});
